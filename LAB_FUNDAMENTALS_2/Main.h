6c148c2c9fa3a152e9292afb5fe806088ea8562b
hohoa <hohoa@LLOONNGGG>
2023-06-18 00:23:07 -0400

    framework

---
+++ LAB_FUNDAMENTALS_2/Main.h
@@ -0,0 +1,11 @@
+#pragma once
+#include<SDL.h>
+#include<SDL_image.h>
+#include<SDL_mixer.h>
+#include<ctime>
+#include<Windows.h>
+#include<iostream>
+using namespace std;
+
+static int ScreenWidth = 800;
+static int ScreenHeight = 600;
